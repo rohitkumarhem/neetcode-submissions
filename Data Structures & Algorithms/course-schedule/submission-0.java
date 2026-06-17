dd8eac3c61c4e0cfb9acf536ccde7780f6d0d311
rohitkumarhem <66822414+rohitkumarhem@users.noreply.github.com>
2026-06-17 10:20:48 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.java
@@ -0,0 +1,55 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for(int i=0;i<numCourses;i++)
+        {
+            graph.add(new ArrayList<>());
+        }
+
+        int []indegree = new int[numCourses];
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int []p : prerequisites)
+        {
+            int course = p[0];
+            int pre = p[1];
+
+             graph.get(pre).add(course);
+
+             indegree[course]++;
+        }
+
+        for(int i=0;i<numCourses;i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.offer(i);
+            }
+        }
+
+        int count = 0;
+
+        while(!q.isEmpty())
+        {
+            int curr = q.poll();
+
+            count++;
+
+            for(int next : graph.get(curr))
+            {
+                indegree[next]--;
+
+                if(indegree[next]==0)
+                {
+                    q.offer(next);
+                }
+            }
+        }
+
+        return count == numCourses;
+
+    }
+}
